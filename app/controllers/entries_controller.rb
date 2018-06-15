7461dc1fe7cf280b6aa8d9dbe302196e72205f8f
hlerman <haguyle@gmail.com>
2018-06-14 20:08:33 -0400

    added a show view under entries with a placeholder message using ERB

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,8 +1,13 @@
 class EntriesController < ApplicationController
     def index
+        # here we'll define some @instance_variables to store data from the database for the views to use
          render :index
     end
 
+    def show
+        #show page usually shows a single instance of that type of data
+        render :show
+    end
 
 
 
