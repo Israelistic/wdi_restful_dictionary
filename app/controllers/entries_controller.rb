6772eb2a69b7324c14387bf6b74692ab79f59b49
Israelistic <haguyle@gmail.com>
2018-06-19 16:24:17 -0400

    Added the methods in the controller. added edit page and the option feture

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,26 +2,50 @@ class EntriesController < ApplicationController
     def index
         # here we'll define some @instance_variables to store data from the database for the views to use
         # render :index
+
+        @enteries = Entry.all
     end
 
     def show
         #show page usually shows a single instance of that type of data
+        @entry = Entry.find(params[:id])
         render :show
     end
     def new
-        render :new
+        @entry = Entry.new
     end
     def create
-        redirect_to enteries_url
+        @entry = Entry.new
+        @entry.word = params[:entry][:word]
+        @entry.definition = params[:entry][:definition]
+        @entry.language = params[:entry][:language]
+
+        if @entry.save
+            redirect_to entries_url
+        else
+            redirect_to new_entry_url
+        end
     end
     def edit
-        render :edit
+        @entry = Entry.find(params[:id])
     end
     def update
-        redirect_to entry_url(params[:id])
+        @entry = Entry.find(params[:id])
+
+        @entry.word = params[:entry][:word]
+        @entry.definition = params[:entry][:definition]
+        @entry.language = params[:entry][:language]
+
+        if @entry.save
+            redirect_to entries_url
+        else
+            redirect_to edit_entry_url(@entry)
+        end
     end
     def destroy
-        redirect_to "/index"
+        @entry = Entry.find(params[:id])
+        @entry.destroy
+        redirect_to entries_url
     end
 
 end
